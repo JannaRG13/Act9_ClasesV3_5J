d2aa216fa36cc0acf4149181e41fe1f50d7389b5
Janna Ramirez Granados <a.22308051281287@cbtis128.edu.mx>
2024-09-20 09:32:36 -0600

    Add files via upload

---
+++ OperadoresA.py
@@ -0,0 +1,46 @@
+print ("-- ACTIVIDAD 9 CLASES VERSION 3 --")
+print ("Janna Ramirez Mat: 22308051281287")
+
+# ZONA DE CLASS
+class Operadores1287:
+    # ZONA DE FUNCIONES 
+    def suma1287(self,J,R):
+        s1287=J+R
+        print(f"La suma de {J}+{R}={s1287}")
+    def resta1287(self,J,R):
+        r1287=J-R
+        print(f"La resta de {J}-{R}={r1287}")
+    def multiplicacion1287(self,J,R):
+        m1287=J*R
+        print(f"La multiplicacion de {J}x{R}={m1287}")
+    def division1287(self,J,R):
+        d1287=J/R
+        print(f"La division de {J}/{R}={d1287}")
+    def modulo1287(self,J,R):
+        mod1287=J%R
+        print(f"El modulo de {J}%{R}={mod1287}")
+    def exponente1287(self,J,R):
+        exp1287=J**R
+        print(f"El exponente de {J}**{R}={exp1287}")
+    def cociente1287(self,J,R):
+        cos1287=J//R
+        print(f"El cociente de {J}//{R}={cos1287}")
+# CREACION DEL OBJETO 
+operajanna=Operadores1287()
+
+
+# ZONA DE USO DE OBJETOS
+print(" --- FUNCIÓN SUMA --- ")
+operajanna.suma1287(2,6)
+print(" --- FUNCIÓN RESTA --- ")
+operajanna.resta1287(2,6)
+print(" --- FUNCIÓN MULTIPLICACION --- ")
+operajanna.multiplicacion1287(2,6)
+print(" --- FUNCIÓN DIVISION --- ")
+operajanna.division1287(2,6)
+print(" --- FUNCIÓN MODULO --- ")
+operajanna.modulo1287(2,6)
+print(" --- FUNCIÓN EXPONENTE --- ")
+operajanna.exponente1287(2,6)
+print(" --- FUNCIÓN COCIENTE --- ")
+operajanna.cociente1287(2,6)
